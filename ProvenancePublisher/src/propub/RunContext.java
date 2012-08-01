94e2b21ab6722cd07b189ee8416af357f5a3d1ae
Sean Riddle <swriddle@gmail.com>
2012-08-01 10:02:24 -0700

    Improved RunContext so it can fully encapsulate the information for a model.

--- ProvenancePublisher/src/propub/RunContext.java
+++ ProvenancePublisher/src/propub/RunContext.java
@@ -1,6 +1,12 @@
 package propub;
 
+import db.DLVDriver;
+import env.EnvInfo;
+import file.FileDriver;
+import parser.Model;
+
 import java.io.File;
+import java.io.IOException;
 
 // This class encapsulates everything about the current state of the
 // graph, including User Requests (both security and explanatory), and
@@ -11,21 +17,41 @@ public class RunContext {
 	// This constructor is for a context with an input file, but no user
 	// requests. Consider renaming this to a public static method so the name
 	// can provide some context.
-	public RunContext(File file) {
-		Constants constants = new Constants();
-		DLVDriver dlv = new DLVDriver();
-		dlv.exeDLV(new String[] {constants.SL_DLV_PATH, file.getAbsolutePath(), blankFile()});
-
-		model = fd.readFile(constants.PROPUB_EXE + constants.ENV_SEPARATOR + "out.txt").toString();
-		model = model.replaceAll("\\),", "\\).").replace("}", ".");
-
-		this.model = new Model(model);
-
-		//display image
-//		displayImage(model.getIntrmediateModel(model.getFinalStateNo()));
+	public RunContext(File pgFile) {
+        this(pgFile, blankFile());
 	}
 
-	public RunContext(RunContext rc,
+    public RunContext(File pgFile, File urFile) {
+        FileDriver fd = new FileDriver();
+        Constants constants = new Constants();
+        EnvInfo ei = new EnvInfo();
+        ei.setSetupInfo(constants);
+        DLVDriver dlv = new DLVDriver();
+        dlv.exeDLV(new String[] {constants.SL_DLV_PATH, pgFile.getAbsolutePath(), urFile.getAbsolutePath()});
+
+        String modelString = fd.readFile(constants.PROPUB_EXE + constants.ENV_SEPARATOR + "out.txt").toString();
+        modelString = modelString.replaceAll("\\),", "\\).").replace("}", ".");
+
+        this.pgFile = pgFile;
+        this.urFile = blankFile();
+        this.model = new Model(modelString);
+
+        //display image
+//		displayImage(model.getIntrmediateModel(model.getFinalStateNo()));
+    }
+
+    public File getUserRequestFile() {
+        return urFile;
+    }
+
+    public File getProgramGraphFile() {
+        return pgFile;
+    }
+
+	public RunContext withNewUserRequestFile(File userRequestFile) {
+        File pgFile = this.getProgramGraphFile();
+        return new RunContext(pgFile, userRequestFile);
+    }
 
 	private static File blankFile() {
 		if (blankFile == null) {
@@ -38,4 +64,13 @@ public class RunContext {
 		}
 		return blankFile;
 	}
+
+    private Model model = null;
+    private File pgFile = null;
+    private File urFile = null;
+    private static File blankFile = null;
+
+    public Model getModel() {
+        return model;
+    }
 }
