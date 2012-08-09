7552d5851e5b2b90f1f35810ad1f25497508d663
Sean Riddle <swriddle@gmail.com>
2012-08-09 11:39:26 -0700

    Substantial changes in order to fix the bug of setting not allowing further queries. It turns out, the bug was that the model that was generated was not syntactically valid, so all future queries failed quietly. Something to add would be visual feedback of the DLV execution, basically just a way to communicate to the user anything that came out on stderr.

--- ProvenancePublisher/src/propub/RunContext.java
+++ ProvenancePublisher/src/propub/RunContext.java
@@ -6,7 +6,19 @@ import file.FileDriver;
 import parser.Model;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // This class encapsulates everything about the current state of the
 // graph, including User Requests (both security and explanatory), and
@@ -31,12 +43,10 @@ public class RunContext {
         EnvInfo ei = new EnvInfo();
         ei.setSetupInfo(constants);
         DLVDriver dlv = new DLVDriver();
-        if (!urFile.getName().startsWith("blank")) {
-            System.out.println("Whoa");
-        }
         dlv.exeDLV(new String[] {constants.SL_DLV_PATH, pgFile.getAbsolutePath(), urFile.getAbsolutePath()});
 
         String modelString = fd.readFile(constants.PROPUB_EXE + constants.ENV_SEPARATOR + "out.txt").toString();
+
         modelString = modelString.replaceAll("\\),", "\\).").replace("}", ".");
 
         this.pgFile = pgFile;
@@ -64,9 +74,38 @@ public class RunContext {
         return pgFile;
     }
 
-	public RunContext withNewUserRequestFile(File userRequestFile) {
-        File pgFile = this.getProgramGraphFile();
-        return new RunContext(pgFile, userRequestFile);
+    public static File writeOut(String s) {
+        File f = null;
+        Writer w = null;
+        try {
+            f = File.createTempFile("blahblah", ".dlv");
+            w = new FileWriter(f);
+            w.write(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (w != null) {
+                try {
+                    w.close();
+                }
+                catch(IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        return f;
+    }
+
+	public RunContext withNewUserRequestFile(File userRequestFile, int state) {
+        if (fileLoadContext) {
+            return new RunContext(pgFile, userRequestFile);
+        }
+        else {
+            File pgFile = processModelOutput();
+            return new RunContext(pgFile, userRequestFile);
+        }
     }
 
 	private static File blankFile() {
@@ -81,7 +120,132 @@ public class RunContext {
 		return blankFile;
 	}
 
+    Pattern PATTERN_PREDICATE_WITH_ARGUMENTS = Pattern.compile("([a-zA-Z_]+)\\((.*)\\)\\.");
+
+	private File processModelOutput() {
+
+        Set<String> interestingPredicates = new HashSet<String>(Arrays.asList("l_data", "l_actor", "l_gen_by", "l_used", "l_dep"));
+
+		String modelString = model.getModel();
+        if (modelString.charAt(0) == '{') {
+            modelString = modelString.substring(1);     // Cut off the last character.
+        }
+        String[] modelComponents = modelString.split("\\s+");
+
+        Map<Integer, List<String>> dataByState = new HashMap<Integer, List<String>>();
+        Map<Integer, List<String>> actorByState = new HashMap<Integer, List<String>>();
+        Map<Integer, List<String>> genByByState = new HashMap<Integer, List<String>>();
+        Map<Integer, List<String>> usedByState = new HashMap<Integer, List<String>>();
+        Map<Integer, List<String>> depByState = new HashMap<Integer, List<String>>();
+
+        for (String modelComp : modelComponents) {
+            if (!modelComp.endsWith(".")) {
+                continue;       // Throw out weirdness like all_lineage until I find out what it does.
+                // At the moment, this just ends up throwing out all zero arity predicates.
+            }
+            Matcher m = PATTERN_PREDICATE_WITH_ARGUMENTS.matcher(modelComp);
+            if (m.matches()) {
+                String predicate = m.group(1);
+                String arguments = m.group(2);
+                if (!interestingPredicates.contains(predicate)) {
+                    System.out.println(predicate + " not in group.");
+                    // Sorry, not interested.
+                    continue;
+                }
+                String[] argumentPieces = arguments.split(",");
+                String stageString = argumentPieces[argumentPieces.length - 1];
+                int stageNum = Integer.valueOf(stageString);
+
+                if ("l_data".equals(predicate)) {
+                    // add to map, indexed by stage
+                    if (!dataByState.containsKey(stageNum)) {
+                        dataByState.put(stageNum, new ArrayList<String>());
+                    }
+                    dataByState.get(stageNum).add("data(" + argumentPieces[0] + "," + argumentPieces[1] + ").");
+                }
+                else if ("l_dep".equals(predicate)) {
+                    if (!depByState.containsKey(stageNum)) {
+                        depByState.put(stageNum, new ArrayList<String>());
+                    }
+                    depByState.get(stageNum).add("dep(" + argumentPieces[0] + "," + argumentPieces[1] + ").");
+                }
+                else if ("l_actor".equals(predicate)) {
+                    if (!actorByState.containsKey(stageNum)) {
+                        actorByState.put(stageNum, new ArrayList<String>());
+                    }
+                    actorByState.get(stageNum).add("actor(" + argumentPieces[0] + "," + argumentPieces[1] + ").");
+                }
+                else if ("l_gen_by".equals(predicate)) {
+                    if (!genByByState.containsKey(stageNum)) {
+                        genByByState.put(stageNum, new ArrayList<String>());
+                    }
+                    genByByState.get(stageNum).add("gen_by(" + argumentPieces[0] + "," + argumentPieces[1] + ").");
+                }
+                else if ("l_used".equals(predicate)) {
+                    if (!usedByState.containsKey(stageNum)) {
+                        usedByState.put(stageNum, new ArrayList<String>());
+                    }
+                    usedByState.get(stageNum).add("used(" + argumentPieces[0] + "," + argumentPieces[1] + ").");
+                }
+            }
+
+        }
+
+        if (dataByState.isEmpty() && actorByState.isEmpty() && genByByState.isEmpty() && usedByState.isEmpty()) {
+            System.out.println("No new information in this model output, nothing to rewrite. Using old program graph file...");
+            return pgFile;
+        }
+
+        // Use the maximum stage recorded, for now. Preferably, this should be configurable.
+        int max1 = Collections.max(dataByState.keySet());
+        int max2 = Collections.max(actorByState.keySet());
+        int max3 = Collections.max(genByByState.keySet());
+        int max4 = Collections.max(usedByState.keySet());
+        int max5 = Collections.max(depByState.keySet());
+
+        int max = max1 > max2 ? max1 : max2;
+        max = max > max3 ? max : max3;
+        max = max > max4 ? max : max4;
+        max = max > max5 ? max : max5;
+        System.out.println("Maximum stage computed: " + max + " (" + max1 + "," + max2 + "," + max3 + "," + max4 + "," + max5 + ")");
+       // modelString.split()
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String item : dataByState.get(max)) {
+            sb.append(item).append(" ");
+        }
+        for (String item : actorByState.get(max)) {
+            sb.append(item).append(" ");
+        }
+        for (String item : genByByState.get(max)) {
+            sb.append(item).append(" ");
+        }
+        for (String item : usedByState.get(max)) {
+            sb.append(item).append(" ");
+        }
+
+        // SEAN: Need to do this to fix up the syntax to be *exactly* what DLV
+        // wants.
+        // It is very picky and will choke with a syntax error on even an extra
+        // period at the end. A more principled way of cleaning the file up, or
+        // of contructing it in the first place, would be desirable
+        while (sb.charAt(sb.length() - 1) != ')') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        sb.append(".");
+
+        File newPgFile = writeOut(sb.toString());
+
+        return newPgFile;
+	}
+
+    public void setFileLoadContext(boolean b) {
+        fileLoadContext = b;
+    }
+
     private Model model = null;
+    private boolean fileLoadContext = false;
     private File pgFile = null;
     private File urFile = null;
     private static File blankFile = null;
