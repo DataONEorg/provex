8768abedf22b77b1e3f5eb9e846786fefb3287f8
Sean Riddle <swriddle@ucdavis.edu>
2012-09-28 14:45:23 -0700

    Got the table displaying correctly, with centered checkboxes corresponding to a centered header checkmark

--- ProvenancePublisher/src/propub/MyJTable.java
+++ ProvenancePublisher/src/propub/MyJTable.java
@@ -1,13 +1,24 @@
 package propub;
 
-import javax.swing.JTable;
-import javax.swing.table.TableModel;
-import java.awt.Dimension;
+import javax.swing.*;
+import javax.swing.event.TableColumnModelListener;
+import javax.swing.table.*;
+import java.awt.*;
+import java.util.Enumeration;
 
 public class MyJTable extends JTable {
 
 	public MyJTable(TableModel model) {
 		super(model);
+		TableColumnModel columnModel = this.getColumnModel();
+		columnModel.getColumn(0).setPreferredWidth(10);
+		_center(columnModel.getColumn(0));
+	}
+
+	private void _center(TableColumn column) {
+
+		MyTableCellRenderer renderer = new MyTableCellRenderer(this);
+		column.setHeaderRenderer(renderer);
 	}
 
 	@Override
@@ -15,5 +26,20 @@ public class MyJTable extends JTable {
 		return new Dimension(225, 120);
 	}
 
+	public static class MyTableCellRenderer implements TableCellRenderer {
+		public MyTableCellRenderer(JTable table) {
+			this.myTable = table;
+		}
+
+		public Component getTableCellRendererComponent(JTable table, Object o, boolean isS, boolean hasF, int row, int col) {
+			TableCellRenderer tcr = myTable.getTableHeader().getDefaultRenderer();
+			JLabel l = (JLabel) tcr.getTableCellRendererComponent(myTable, null, isS, hasF, row, col);
+			l.setText("\u2714");
+			l.setHorizontalAlignment(SwingConstants.CENTER);
+			return l;
+		}
+
+		final private JTable myTable;
+	}
 }
 
