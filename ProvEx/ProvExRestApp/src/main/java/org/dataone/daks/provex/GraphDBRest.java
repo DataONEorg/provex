a0587995fab0621568c15759cab7e2da009c8e33
Victor <Victor@Victor-PC>
2012-12-24 16:56:52 -0800

    Added the GraphDBRest service.

---
+++ ProvEx/ProvExRestApp/src/main/java/org/dataone/daks/provex/GraphDBRest.java
@@ -0,0 +1,31 @@
+
+package org.dataone.daks.provex;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.dataone.daks.rpq.RPQDBDAO;
+
+
+/** Example resource class hosted at the URI path "/graphdbrest"
+ */
+@Path("/graphdbrest")
+public class GraphDBRest {
+    
+    /** Method processing HTTP GET requests, producing "text/plain" MIME media
+     * type.
+     * @return String that will be send back as a response of type "text/plain".
+     */
+    @GET 
+    @Produces("text/plain")
+    public String getGraphJSON() {
+    	RPQDBDAO dao = RPQDBDAO.getInstance();
+    	String graphJSON = dao.getGraphJSON("graph");
+        return "The graph is: \n" + graphJSON;
+    }
+    
+    
+}
+
+
---
+++ ProvEx/ProvExRestApp/src/main/java/org/dataone/daks/provex/RPQEngineRest.java
@@ -0,0 +1,49 @@
+
+package org.dataone.daks.provex;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import org.dataone.daks.rpq.RPQEngine;
+import java.io.*;
+
+/** Example resource class hosted at the URI path "/rpqenginerest"
+ */
+@Path("/rpqenginerest")
+public class RPQEngineRest {
+    
+    /** Method processing HTTP GET requests, producing "text/plain" MIME media
+     * type.
+     * @return String that will be send back as a response of type "text/plain".
+     */
+    @GET 
+    @Produces("text/plain")
+    public String runQuery(@QueryParam("q") String query) {
+    	RPQEngine engine = new RPQEngine();
+    	//Usage: java RPQEngine (query) (tablename) (config file) [-4 for returning paths] [-JSON]
+    	engine.main(new String[]{query, "graph", "config", "-JSON"});
+        return "The query is: " + query + "\n\nThe result is: \n\n" + readFile("results.txt");
+    }
+    
+    
+    private String readFile(String filename) {
+    	BufferedReader input = null;
+    	StringBuffer buff = new StringBuffer();
+    	String line = null;
+    	try {
+    		input =  new BufferedReader(new FileReader(new File(filename)));
+    		while( (line = input.readLine()) != null)
+    			buff.append(line);
+    	}
+    	catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    	return buff.toString();
+    }
+    
+    
+}
+
+
