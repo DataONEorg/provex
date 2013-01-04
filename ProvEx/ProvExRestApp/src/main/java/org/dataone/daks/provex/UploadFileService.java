720e9bde17acc38fcac59e40d25b4b67192d82e6
Victor <Victor@Victor-PC>
2013-01-04 13:36:46 -0800

    Added the file and graph upload services.

---
+++ ProvEx/ProvExRestApp/src/main/java/org/dataone/daks/provex/UploadFileService.java
@@ -0,0 +1,60 @@
+package org.dataone.daks.provex;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
+
+@Path("/fileupload")
+public class UploadFileService {
+
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(
+			@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail,
+			@FormDataParam("param1") String param1,
+			@FormDataParam("param2") String param2) {
+
+		System.out.println("Param1: " + param1);
+		System.out.println("Param2: " + param2);
+		
+		String uploadedFileName = "upload.txt";
+
+		writeToFile(uploadedInputStream, uploadedFileName);
+
+		String output = "File uploaded to : " + uploadedFileName;
+
+		return Response.status(200).entity(output).build();
+	}
+
+
+	private void writeToFile(InputStream uploadedInputStream, String uploadedFileName) {
+		try {
+			OutputStream out = new FileOutputStream(new File(uploadedFileName));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+			out = new FileOutputStream(new File(uploadedFileName));
+			while ((read = uploadedInputStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
