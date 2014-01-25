4a0f7ec7d30de1262124816e981ae487272eadf4
Victor <Victor@Victor-PC>
2014-01-24 21:09:28 -0800

    Corrected the URL and namespace for the provone ontology.

--- ProvXML2RDF/README.txt
+++ ProvXML2RDF/README.txt
@@ -1,3 +1,3 @@
 To execute the transform ProvXML into Neo4j JSON use the command
 
-mvn exec:java -Dexec.mainClass="org.dataone.daks.provxml2json.ProvXML2JSON" -Dexec.args="evaWorkflow1paperPROV.xml"     
+mvn exec:java -Dexec.mainClass="org.dataone.daks.provxml2json.JenaExample" -Dexec.args="evaWorkflow1paperPROV.xml"     
--- ProvXML2RDF/src/main/java/org/dataone/daks/provxml2rdf/JenaExample.java
+++ ProvXML2RDF/src/main/java/org/dataone/daks/provxml2rdf/JenaExample.java
@@ -26,8 +26,8 @@ public class JenaExample {
 			e.printStackTrace();
 		}
 		model = RDFDataMgr.loadModel("wf.xml");
-        //RDFDataMgr.write(System.out, model, Lang.TURTLE);
-        RDFDataMgr.write(System.out, model, RDFFormat.TURTLE_PRETTY);
+		//RDFDataMgr.write(System.out, model, Lang.TURTLE);
+		RDFDataMgr.write(System.out, model, RDFFormat.TURTLE_PRETTY);
 	}
 	
 	
@@ -46,19 +46,19 @@ public class JenaExample {
 		String DCTERMS_NS = "http://purl.org/dc/terms/";
 		String EXAMPLE_NS = "http://example.com/";
 		String WFMS_NS = "http://www.vistrails.org/registry.xsd";
-		//String SOURCE_URL = "http://vcvcomputing.com/provone/provone.owl";
-		String SOURCE_URL = "http://www.semanticweb.org/dataone/ontologies/provone";
+		String SOURCE_URL = "http://purl.org/provone/ontology";
 		String SOURCE_FILE = "./provone.owl";
 
 		OntModel m = ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM );
+		//Uncomment to use file instead of the URL
 		FileManager.get().getLocationMapper().addAltEntry( SOURCE_URL, SOURCE_FILE );
-        Model baseOntology = FileManager.get().loadModel( SOURCE_URL );
-        m.addSubModel( baseOntology );
-        m.setNsPrefix( "provone", SOURCE_URL + "#" );
+		Model baseOntology = FileManager.get().loadModel( SOURCE_URL );
+		m.addSubModel( baseOntology );
+		m.setNsPrefix( "provone", SOURCE_URL + "#" );
         
-        OntClass processClass = m.getOntClass( SOURCE_URL + "#" + "Process" );
-        Individual process_1 = m.createIndividual( EXAMPLE_NS + "process_1", processClass );
-        //Individual process_1 = m.createIndividual( SOURCE_URL + "#" + "process_1", processClass );
+		OntClass processClass = m.getOntClass( SOURCE_URL + "#" + "Process" );
+		Individual process_1 = m.createIndividual( EXAMPLE_NS + "process_1", processClass );
+		//Individual process_1 = m.createIndividual( SOURCE_URL + "#" + "process_1", processClass );
 		
 		Property identifierP = m.createProperty(DCTERMS_NS + "identifier");
 		process_1.addProperty(identifierP, "e1", XSDDatatype.XSDstring);
